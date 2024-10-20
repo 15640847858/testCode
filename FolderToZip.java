32ca7aeb5fd7417027dbef2ec58dbc52873f5dfb
Weiwen Song <76939819+15640847858@users.noreply.github.com>
2024-10-20 14:33:39 +0900

    Update FolderToZip.java

--- FolderToZip.java
+++ FolderToZip.java
@@ -1,4 +1,4 @@
-﻿import java.io.*;
+import java.io.*;
 import java.nio.file.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -7,22 +7,22 @@ import java.util.zip.ZipOutputStream;
 
 public class FolderToZip {
 
-    // 创建文件夹结构
+    // フォルダ構造を作成する
     public static void createFolders(String caseNumber, List<String> receptionNumbers, Map<String, List<String>> filesMap) throws IOException {
-        // 创建主文件夹（案件番号）
+        // 主フォルダ（案件番号）を作成
         File caseDir = new File(caseNumber);
         if (!caseDir.exists()) {
             caseDir.mkdir();
         }
 
-        // 创建多个子文件夹（受付番号）
+        // 複数のサブフォルダ（受付番号）を作成
         for (String receptionNumber : receptionNumbers) {
             File receptionDir = new File(caseDir, receptionNumber);
             if (!receptionDir.exists()) {
                 receptionDir.mkdir();
             }
 
-            // 在子文件夹中创建多个文件（添付文件）
+            // サブフォルダに複数のファイル（添付ファイル）を作成
             List<String> fileNames = filesMap.get(receptionNumber);
             if (fileNames != null) {
                 for (String fileName : fileNames) {
@@ -35,7 +35,7 @@ public class FolderToZip {
         }
     }
 
-    // 压缩文件夹到ZIP
+    // フォルダをZIPに圧縮する
     public static void zipDirectory(File folder, File zipFile) throws IOException {
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile))) {
             zipFolder(folder, folder.getName(), zos);
@@ -46,10 +46,10 @@ public class FolderToZip {
         File[] files = folder.listFiles();
         for (File file : files) {
             if (file.isDirectory()) {
-                // 递归处理文件夹
+                // フォルダを再帰的に処理する
                 zipFolder(file, parentFolder + "/" + file.getName(), zos);
             } else {
-                // 压缩文件
+                // ファイルを圧縮する
                 try (FileInputStream fis = new FileInputStream(file)) {
                     ZipEntry zipEntry = new ZipEntry(parentFolder + "/" + file.getName());
                     zos.putNextEntry(zipEntry);
@@ -66,32 +66,32 @@ public class FolderToZip {
         }
     }
 
-    // 获取当前时间字符串
+    // 現在の日時の文字列を取得する
     public static String getCurrentTimestamp() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
         return sdf.format(new Date());
     }
 
     public static void main(String[] args) throws IOException {
-        // 输入：案件番号、受付番号、添付文件列表
+        // 入力: 案件番号、受付番号、添付ファイルリスト
         String caseNumber = "Case123";
         List<String> receptionNumbers = Arrays.asList("Reception001", "Reception002");
 
-        // 每个受付番号对应的文件名列表
+        // 各受付番号に対応するファイル名リスト
         Map<String, List<String>> filesMap = new HashMap<>();
         filesMap.put("Reception001", Arrays.asList("file1.txt", "file2.txt"));
         filesMap.put("Reception002", Arrays.asList("file3.txt", "file4.txt"));
 
-        // 创建文件夹和文件
+        // フォルダとファイルを作成
         createFolders(caseNumber, receptionNumbers, filesMap);
 
-        // 获取当前时间并创建ZIP文件名
+        // 現在の日時を取得してZIPファイル名を作成
         String zipFileName = getCurrentTimestamp() + ".zip";
         File zipFile = new File(zipFileName);
 
-        // 压缩主文件夹
+        // 主フォルダを圧縮
         zipDirectory(new File(caseNumber), zipFile);
 
-        System.out.println("ZIP file created: " + zipFile.getAbsolutePath());
+        System.out.println("ZIPファイルが作成されました: " + zipFile.getAbsolutePath());
     }
 }
